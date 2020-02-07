995047c8296756d43ed9cf5bf2491ee86aadfaa7
Alison Ludick <alison.ludick@gmail.com>
2020-02-07 19:52:39 +0200

    deleted duplicate folders, refactored code in remaining folders

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,81 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import WarsList from './components/StarWarsList';
+class App extends Component {
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  constructor() {
+    super();
+    this.state = {
+      starwarsChars: [],
+      next:null,
+      previous:null,
+      selected:[],
+      loading:true
+    };
+  }
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
+  componentDidMount() {
+    setTimeout(()=> this.getCharacters("https://swapi.co/api/people/"),5000)
+    
+  }
+ 
+  getCharacters = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ 
+          starwarsChars: data.results ,
+          next: data.next ,
+          previous:data.previous,
+          loading:false,
+        });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+  nextPage = (e)=>{
+    return this.getCharacters(e)
+  }
+  previousPage =(e) =>{
+    return this.getCharacters(e)
+  }
+  toggle =(e) =>{
+    const selectedChar = this.state.starwarsChars.find(starwarsChar => starwarsChar.url === e )
+    const newSelected = Object.assign({} ,this.state );
+    if(newSelected.selected.includes(selectedChar.url)){
+      const index = newSelected.selected.indexOf(selectedChar.url)
+      newSelected.selected.pop(index)
+      this.setState({
+         selected:newSelected.selected
+       })
+    }
+    else{
+      newSelected.selected.push(selectedChar.url)
+      this.setState({
+           selected:newSelected.selected
+        })
+    }
+}
+  render() {
+    return (
+     
+      <div className="App">
+        <h1 className="Header">React Wars</h1> 
+        <WarsList 
+          loading ={this.state.loading}
+          list={this.state.starwarsChars}  
+          nextPage={this.nextPage}  
+          previousPage ={this.previousPage} 
+          next={this.state.next} 
+          previous={this.state.previous} 
+          onToggle={this.toggle} 
+          selected={this.state.selected}/>
+          </div>
+    );
+  }
 }
 
 export default App;
---
+++ starwars/src/components/StarWarsCharacter.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./StarWars.css";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const StarWarsCharacter = ({
+  name,
+  gender,
+  height,
+  mass,
+  eyeColor,
+  hairColor,
+  skinColor,
+  change,
+  url,
+  select
+}) => {
+    const style = {
+        display: select.includes(url) ?'block':'none',
+
+    }
+    const starStyle = {
+        color: select.includes(url)? 'brown':'black',
+    }
+  return (
+    <div className="character" onClick={() => change(url)} >
+      <div className='character-name'>
+      <h2>{name}</h2>
+      <FontAwesomeIcon  style={starStyle} icon={faStar} />
+      </div>
+      <div className="character-details" style={style}>
+        <p>Gender: {gender}</p>
+        <p>Height: {height}</p>
+        <p>Mass: {mass}</p>
+        <p>Eye Color: {eyeColor}</p>
+        <p>Hair Color: {hairColor}</p>
+        <p>Skin Color: {skinColor}</p>
+      </div>
+    </div>
+  );
+};
+export default StarWarsCharacter;
