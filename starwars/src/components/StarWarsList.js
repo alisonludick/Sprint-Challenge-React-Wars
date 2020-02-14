0b091dd6bd9a3468355cf8e0d329f81c1129eb41
Alison Ludick <alison.ludick@gmail.com>
2020-02-14 06:32:41 +0200

    refactoring code to implement hooks, and deleting unnecessary folders

--- starwars/src/components/StarWarsList.js
+++ starwars/src/components/StarWarsList.js
@@ -1,41 +1,20 @@
 import React from "react";
-import "./StarWars.css";
-import Characters from './StarWarsCharacter';
+import StarWarsCharacter from "./StarWarsCharacter";
 
-const StarWarsList = ({ list  , nextPage , previousPage,next ,previous,onToggle, selected,loading }) => {
-    const style={
-        display: loading ?'block':'none'
-    }
-    const anotherstyle={
-        display: loading ?'none':'block'
-    }
-  return (
-  
-    <div className="characterList">
-      <div className = 'initial' style={style}>
-    </div>
-    <h1 style={anotherstyle}>Star Wars Characters</h1>
-      {list.map(todo => (
-        <Characters 
-          key={todo.url}
-          name={todo.name}
-          birthYear={todo.birth_year}
-          gender={todo.gender}
-          height={todo.height}
-          mass={todo.mass}
-          eyeColor={todo.eye_color}
-          hairColor={todo.hair_color}
-          skinColor={todo.skin_color}
-          url={todo.url}
-          select={selected}
-          change={onToggle}
-        />
-      ))}
-      <div>
-      { previous ? <button className ='button' onClick={() =>previousPage(previous)}>Previous</button> : null}
-      {next ? <button className ='button'  onClick={() =>nextPage(next)}>Next</button> :null}
-      </div>
-    </div>
-  );
-};
-export default StarWarsList;
+function StarWarsList(props) {
+  return props.characterArray.map(character => (
+    <StarWarsList
+      key={character.name}
+      name={character.name}
+      birth_year={character.birth_year}
+      gender={character.gender}
+      skin_color={character.skin_color}
+      eye_color={character.eye_color}
+      hair_color={character.hair_color}
+      height={character.height}
+      mass={character.mass}
+    />
+  ));
+}
+
+export default StarWarsCharacter;
